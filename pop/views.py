1a472f8487a64566f3ca782b07e2d85a9531ef77
xiangwb <xiangwenbin521@gmail.com>
2016-04-15 09:16:06 +0800

    first commit

--- pop/views.py
+++ pop/views.py
@@ -6,8 +6,10 @@ def index(request):
 	return HttpResponse("Hello,world. You're at the Pop index page.")
 
 def catagory_list(request):
-	catagory_list=Catagory.objects.all()
-	return HttpResponse("Hello,world. You're at the catagory list page"+'\n'+catagory_list)
+	catagory=Catagory.objects.all()[0].catagory
+	response="Hello,world. You're at the catagory list page"+'\n'+catagory
+	print response
+	return HttpResponse(response)
 
 def catagory(request):
 	return HttpResponse("Hello,world. You're at the Pop catagory page.")
